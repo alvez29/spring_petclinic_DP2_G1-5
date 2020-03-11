08574d6fe3330cf1bdb5d03f9cc1d3c29d30e9c9
Pablo de los Santos Carrión <pablodelossantoscarrion@gmail.com>
2020-03-11 01:53:25 +0100

    Formulario de carrera terminado. Falta la validación

--- src/main/java/org/springframework/samples/petclinic/service/RaceService.java
+++ src/main/java/org/springframework/samples/petclinic/service/RaceService.java
@@ -1,8 +1,38 @@
 package org.springframework.samples.petclinic.service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.model.Race;
+import org.springframework.samples.petclinic.repository.PetRepository;
+import org.springframework.samples.petclinic.repository.springdatajpa.RaceRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RaceService {
+	
+	private RaceRepository raceRepo;
+	
+	private PetRepository petRepo;
+	
+	@Autowired
+	public RaceService(RaceRepository raceRepository, PetRepository petRepository) {
+		this.petRepo = petRepository;
+		this.raceRepo = raceRepository;
+	}
 
+	@Transactional
+	public void saveRace(@Valid Race race) {
+		raceRepo.save(race);
+	}
+
+	@Transactional
+	public List<PetType> getAllTypes(){
+		return petRepo.findPetTypes();
+		
+	}
 }
