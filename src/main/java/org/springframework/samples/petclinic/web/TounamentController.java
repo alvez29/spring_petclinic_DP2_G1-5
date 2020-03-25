d4b646fee87eec14c2a397c6113c71862fc1d181
nicpazsar <nicopoliris99@gmail.com>
2020-03-25 11:59:32 +0100

    Add pet to tournament finished

--- src/main/java/org/springframework/samples/petclinic/web/TounamentController.java
+++ src/main/java/org/springframework/samples/petclinic/web/TounamentController.java
@@ -1,7 +1,9 @@
 package org.springframework.samples.petclinic.web;
 
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -45,14 +47,37 @@ public class TounamentController {
 	public String linkPetToTournament(@PathVariable("tournamentId") int tournamentId, @PathVariable("petId") int petId, ModelMap modelMap) throws DataAccessException, DuplicatedPetNameException {
 		Tournament tournament = this.tournamentService.findTournamentById(tournamentId).get();
 		Pet pet = this.petService.findPetById(petId);
-		tournament.addPet(pet);
-		//this.tournamentService.
+		List<Pet> pets = tournament.getPets();
+		if (!pets.contains(pet)) {
+			if (tournament.getBreedRestriction() != null) {
+				if (pet.getType().toString().equals(tournament.getBreedRestriction().toString())) {
+					tournament.addPet(pet);
+					this.tournamentService.saveTournament(tournament);
+				}
+			}else {
+				if (pet.getType() != null) {
+					tournament.addPet(pet);
+					this.tournamentService.saveTournament(tournament);
+				}
+				
+			}
+		}
 		return "redirect:/tournaments/" + tournamentId;
 	}
 	
+	
 	@GetMapping("/pet/tournament/{tournamentId}")
 	public String petListForTournament(@PathVariable("tournamentId") int tournamentId, ModelMap model) {
-		Iterable<Pet> pets = petService.findAll();
+		List<Pet> pets = (List<Pet>) petService.findAll();
+		Tournament tournament = this.tournamentService.findTournamentById(tournamentId).get();
+		if (tournament.getBreedRestriction() != null) {
+			List<Pet> petsBreed = pets.stream().filter(x->!x.getType()
+					.equals(tournament.getBreedRestriction())).collect(Collectors.toList());
+			pets.removeAll(petsBreed);
+			pets.removeAll(tournament.getPets());
+		}else {
+			pets.removeAll(tournament.getPets());
+		}
 		model.addAttribute("pets", pets);
 		model.addAttribute("tournamentId", tournamentId);
 		return "pets/petList";
