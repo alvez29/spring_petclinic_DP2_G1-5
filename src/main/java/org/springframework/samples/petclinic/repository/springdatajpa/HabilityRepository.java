e2ccbca2124be6ea42cfd790a25fe976932b9144
Pablo de los Santos Carrión <pablodelossantoscarrion@gmail.com>
2020-03-29 23:25:31 +0200

    Errores en los tests

--- src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/HabilityRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/HabilityRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.samples.petclinic.model.Hability;
+import org.springframework.samples.petclinic.model.Judge;
 import org.springframework.samples.petclinic.model.Sponsor;
 import org.springframework.stereotype.Repository;
 
@@ -23,4 +24,7 @@ public interface HabilityRepository extends CrudRepository<Hability, Integer> {
 	@Query("SELECT s from Sponsor s where tournament.id = ?1")
 //	@Query(value = "SELECT * FROM Sponsor where TOURNAMENT_ID = ?1", nativeQuery = true)
 	Collection<Sponsor> getSponsors(Integer id);
+
+	@Query("SELECT t.judges from Tournament t where t.id = ?1")
+	Collection<Judge> getJudgesById(Integer id);
 }
